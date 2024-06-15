3f9a09f4256620ef7f34ad494ed9f7a0ea2486e6
vrgdev <vszgalaxy@gmail.com>
2024-06-15 18:48:56 +0200

    add ObjectValidator

---
+++ src/Validator/ObjectValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ObjectValidator
+{
+    public function __construct(
+        private ValidatorInterface $validator,
+    ) {
+    }
+
+    public function isValid(object $object): bool
+    {
+        $errors = $this->validator->validate($object);
+
+        if (count($errors) > 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
