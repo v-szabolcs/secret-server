ff13871680d47e01b522d86291006e7f325195be
vrgdev <vszgalaxy@gmail.com>
2024-06-15 19:08:44 +0200

    validation constraints for SecretPayloadDTO

--- src/DTO/SecretPayloadDTO.php
+++ src/DTO/SecretPayloadDTO.php
@@ -2,11 +2,18 @@
 
 namespace App\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class SecretPayloadDTO
 {
     public function __construct(
+        #[Assert\Length(min: 1, max: 255)]
         public string $secret,
+
+        #[Assert\Positive]
         public int $expireAfterViews,
+
+        #[Assert\PositiveOrZero]
         public int $expireAfter,
     ) {
     }
