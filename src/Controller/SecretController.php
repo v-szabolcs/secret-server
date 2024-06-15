e680848e5eff4f97cd13213705e20a018e8f8355
vrgdev <vszgalaxy@gmail.com>
2024-06-15 19:16:49 +0200

    implement create-secret feature

--- src/Controller/SecretController.php
+++ src/Controller/SecretController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\SecretService;
+use App\Validator\ObjectValidator;
+use App\Mapper\SecretPayloadMapper;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,10 +13,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route(path: '/v1', name: 'app_secret_')]
 class SecretController extends AbstractController
 {
+    public function __construct(
+        private SecretPayloadMapper $secretPayloadMapper,
+        private ObjectValidator $objectValidator,
+        private SecretService $secretService,
+    ) {
+    }
+
     #[Route(path: '/secret', name: 'post', methods: ['POST'])]
-    public function post(): Response
+    public function post(Request $request): Response
     {
-        return $this->json(['test' => 'post route'], 200);
+        $payload = $request->request;
+
+        try {
+            $secretPayloadDTO = $this->secretPayloadMapper->map(
+                $payload->get('secret'),
+                $payload->get('expireAfterViews'),
+                $payload->get('expireAfter'),
+            );
+        } catch (\TypeError) {
+            return $this->json(['test' => 'invalid input'], 405);
+        }
+
+        if (!$this->objectValidator->isValid($secretPayloadDTO)) {
+            return $this->json(['test' => 'invalid input'], 405);
+        }
+
+        try {
+            $secretDTO = $this->secretService->create($secretPayloadDTO);
+        } catch (\TypeError) {
+            return $this->json(['test' => 'invalid input'], 405);
+        }
+
+        return $this->json($secretDTO, 200);
     }
 
     #[Route(path: '/secret/{hash}', name: 'get', methods: ['GET'])]
--- src/Service/SecretService.php
+++ src/Service/SecretService.php
@@ -2,7 +2,61 @@
 
 namespace App\Service;
 
+use App\DTO\SecretDTO;
+use App\Entity\Secret;
+use App\Mapper\SecretMapper;
+use App\DTO\SecretPayloadDTO;
+use Doctrine\ORM\EntityManagerInterface;
+
 class SecretService
 {
-    //
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private SecretMapper $secretMapper,
+    ) {
+    }
+
+    public function create(SecretPayloadDTO $secretPayloadDTO): SecretDTO
+    {
+        $createdAt = new \DateTimeImmutable('now');
+        $expiresAt = $secretPayloadDTO->expireAfter > 0 ? $createdAt->modify('+' . $secretPayloadDTO->expireAfter . ' minutes') : null;
+
+        $secretDTO = new SecretDTO(
+            $this->generateHash(),
+            $secretPayloadDTO->secret,
+            $createdAt,
+            $expiresAt,
+            $secretPayloadDTO->expireAfterViews,
+        );
+
+        return $this->store($secretDTO);
+    }
+
+    private function generateHash(): string
+    {
+        $repository = $this->entityManager->getRepository(Secret::class);
+
+        do {
+            $hash = bin2hex(random_bytes(20));
+        } while ($repository->count(['hash' => $hash]) > 0);
+
+        return $hash;
+    }
+
+    private function store(SecretDTO $secretDTO): SecretDTO
+    {
+        $secret = new Secret();
+
+        $secret->setHash($secretDTO->hash);
+        $secret->setSecretText($secretDTO->secretText);
+        $secret->setCreatedAt($secretDTO->createdAt);
+        $secret->setExpiresAt($secretDTO->expiresAt);
+        $secret->setRemainingViews($secretDTO->remainingViews);
+
+        $this->entityManager->persist($secret);
+
+        $this->entityManager->flush();
+
+        return $this->secretMapper->map($secret);
+    }
 }
